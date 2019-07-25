25759430d79ed660586bb18310f4aa79ab0b7be0
masaibar <masaibar.dev@gmail.com>
2019-07-25 21:42:43 +0900

    とりあえずWebViewでYahoo!トップが表示されるところまで

--- WebViewSample/ViewController.swift
+++ WebViewSample/ViewController.swift
@@ -1,12 +1,30 @@
 import UIKit
+import WebKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, WKUIDelegate {
+
+    @IBOutlet weak var webView: WKWebView!
+
+    private let url: URL = URL(string: "https://www.yahoo.co.jp")!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        webView.load(URLRequest(url: url))
     }
 
+    override func loadView() {
+        super.loadView()
+        let config = WKWebViewConfiguration()
+        webView = WKWebView(
+            frame: .zero,
+            configuration: config
+        )
+        webView.uiDelegate = self
+        view = webView
+    }
 
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
 }
 
